1e99522b443ffdc099b71b07d250bc12efd6e9e3
strwbry <annisamustofa@outlook.com>
2024-09-04 11:12:53 +0700

    refactor: deleted @Service annotation from UsersService

--- src/main/java/com/finalproject/stayease/users/service/UsersService.java
+++ src/main/java/com/finalproject/stayease/users/service/UsersService.java
@@ -2,9 +2,7 @@ package com.finalproject.stayease.users.service;
 
 import com.finalproject.stayease.users.entity.Users;
 import java.util.Optional;
-import org.springframework.stereotype.Service;
 
-@Service
 public interface UsersService {
   void save(Users user);
   Optional<Users> findByEmail(String email);
