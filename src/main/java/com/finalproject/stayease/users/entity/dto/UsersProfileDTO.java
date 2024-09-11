b895d6e85db74141acd9d458f41e1f6975c1c627
strwbry <annisamustofa@outlook.com>
2024-09-11 17:15:27 +0700

    Fix(SE-61): Fix for avatarUrl not mapped correctly

--- src/main/java/com/finalproject/stayease/users/entity/dto/UsersProfileDTO.java
+++ src/main/java/com/finalproject/stayease/users/entity/dto/UsersProfileDTO.java
@@ -14,7 +14,7 @@ public class UsersProfileDTO {
   private String firstName;
   private String lastName;
   private String phoneNumber;
-  private String avatar;
+  private String avatarUrl;
   private Boolean isVerified;
   private Boolean isOAuth2;
   private UserType userType;
@@ -26,7 +26,7 @@ public class UsersProfileDTO {
     this.firstName = user.getFirstName();
     this.lastName = user.getLastName();
     this.phoneNumber = user.getPhoneNumber();
-    this.avatar = user.getAvatar();
+    this.avatarUrl = user.getAvatar();
     this.isVerified = user.getIsVerified();
     this.isOAuth2 = !user.getSocialLogins().isEmpty();
     this.userType = user.getUserType();
